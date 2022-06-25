ce388eda071bb2e982008083382c3f2402c623f0
Andy Lu <andy15lu@gmail.com>
2022-06-25 19:57:20 +0300

    done

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	inputSlice := []rune(input)
+	outputSlice := make([]rune, len(inputSlice))
+	for i := 0; i < len(inputSlice); i++ {
+		outputSlice[i] = inputSlice[len(inputSlice)-1-i]
+	}
+	output = string(outputSlice)
 	return output
 }
